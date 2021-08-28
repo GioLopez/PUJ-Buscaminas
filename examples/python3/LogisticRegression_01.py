7b3eec8c9b624a1c984c610ca6adf0b2679a7bed
Gio0 <Gio0@MacBook-Air-de-Marces.local>
2021-08-28 13:49:30 -0500

    Dizque working

--- examples/python3/LogisticRegression_01.py
+++ examples/python3/LogisticRegression_01.py
@@ -10,6 +10,8 @@ import PUJ.Model.Logistic, PUJ.Regression.MaximumLikelihood
 import PUJ.Normalize, PUJ.Optimizer.GradientDescent
 import PUJ.Debug.Labeling
 
+print('!'*8)
+
 ## -------------------------------------------------------------------------
 learning_rate = 1e-4
 maximum_iterations = 100000
@@ -20,29 +22,34 @@ init_theta = numpy.random.rand( 1, 3 ) * 1e-1
 ## -------------------------------------------------------------------------
 
 ## Read data
-D = numpy.loadtxt( open( sys.argv[ 1 ], 'rb' ), delimiter = ',' )
-numpy.random.shuffle( D )
+# D = numpy.loadtxt( open( sys.argv[ 1 ], 'rb' ), delimiter = ',' )
+csv_data = numpy.loadtxt(
+  open('/Users/Gio0/Develop/Python/Buscaminas/PUJ_ML/examples/python3/algo.csv', 'rb'),
+  delimiter=','
+)
+
+# numpy.random.shuffle(csv_data)
 
 ## -- Separate X and y
-x0 = D[ : , : -1 ]
-y0 = numpy.matrix( D[ : , -1 ] ).T
+x0 = csv_data[:, :-1]
+y0 = numpy.matrix(csv_data[:, -1]).T
 
 # Prepare regression
-x0, x0_off, x0_div = PUJ.Normalize.Standardize( x0 )
+x0, x0_off, x0_div = PUJ.Normalize.Standardize(x0)
 cost = PUJ.Regression.MaximumLikelihood( x0, y0 )
 
 # Prepare debug
-debug = PUJ.Debug.Labeling( x0[ : , 0 : 2 ], y0 )
-
+debug = PUJ.Debug.Labeling(x0[:, 0: 2], y0)
+print('#'*8)
 # Iterative solution
 tI, nI = PUJ.Optimizer.GradientDescent(
   cost,
-  learning_rate = learning_rate,
-  init_theta = init_theta,
-  maximum_iterations = maximum_iterations,
-  epsilon = epsilon,
-  debug_step = debug_step,
-  debug_function = debug
+  learning_rate=learning_rate,
+  init_theta=init_theta,
+  maximum_iterations=maximum_iterations,
+  epsilon=epsilon,
+  debug_step=debug_step,
+  debug_function=None
   )
 
 print( '=================================================================' )
@@ -53,3 +60,6 @@ print( '=================================================================' )
 debug.KeepFigures( )
 
 ## eof - $RCSfile$
+
+if __name__ == "__main__":
+  print('Stating')
--- examples/python3/MineSweeper/Player_Perceptron.py
+++ examples/python3/MineSweeper/Player_Perceptron.py
@@ -1,47 +1,69 @@
 # =========================================================================
 # @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
 # =========================================================================
-
+import random
 import sys, os
 from random import randint
 from MineSweeperBoard import *
 sys.path.append(os.path.join( os.getcwd( ), '../../../lib/python3'))
-from lib.python3.PUJ.Model.Logistic import Logistic as PujLog
+from lib.python3.PUJ.Model.Logistic import Logistic as PujLogistic
 
 # -------------------------------------------------------------------------
 if len(sys.argv) < 4:
     print("Usage: python", sys.argv[0], "width height mines")
     sys.exit(1)
 # end if
-w = int(sys.argv[1])
-h = int(sys.argv[2])
-m = int(sys.argv[3])
-board = MineSweeperBoard(w, h, m)
+width = int(sys.argv[1])
+height = int(sys.argv[2])
+mines = int(sys.argv[3])
+board = MineSweeperBoard(width, height, mines)
 
-weights = [0, 0, 0, 0, 0, 0, 0, 0]
-bias = 0
-LogisticObject = PujLog(weights, bias)
+weights = [-0.67425597, -0.25557734,  1.21907006, -0.5388088,  -0.84763563,  1.2426736, -0.08496626, -0.07428249 ]
+bias = 0.06357287
+logistic_model = PujLogistic(weights, bias)
 
-# Talleres_2021_30_Giovanni_Lopez_Carlos_Diaz
+patches = [[9 for row in range(height)] for columns in range(width)]
+neighbours = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+]
 
-patches = [[0 for j in range(h)] for i in range(w)]
-
-print(patches)
 
 while not board.have_won() and not board.have_lose():
     print(board)
 
+    max_confidence = -1
+    max_column = -1
+    max_row = -1
+    for column in range(width):
+        for row in range(height):
+            if patches[column][row] == 9:
+                list_neighbours = []
 
-    # i = randint(0, board.width() - 1)
-    # j = randint(0, board.height() - 1)
-    # board.click(j, i)
-# end while
+                for neigh in neighbours:
+                    neigh_column = column + neigh[0]
+                    neigh_row = row + neigh[1]
+
+                    if neigh_column < 0 or neigh_row < 0 or neigh_column >= width or neigh_row >= height:
+                        list_neighbours.append(9)
+                    else:
+                        list_neighbours.append(patches[neigh_column][neigh_row])
+
+                confidence = logistic_model(list_neighbours, threshold=False)
+                if confidence > max_confidence:
+                    max_confidence = confidence
+                    max_row = row
+                    max_column = column
+    patches[max_column][max_row] = board.click(max_row, max_column)
 
 print(board)
 if board.have_won():
     print("You won!")
 elif board.have_lose():
     print("You lose :-(")
-# end if
-
-# eof - $RCSfile$
