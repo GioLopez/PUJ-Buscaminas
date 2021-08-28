282f8abfe1e49ab8c9d306c696113357f78f0481
Gio0 <Gio0@MacBook-Air-de-Marces.local>
2021-08-28 09:47:12 -0500

    removed double space and add tab

--- lib/python3/PUJ/Model/Linear.py
+++ lib/python3/PUJ/Model/Linear.py
@@ -6,75 +6,73 @@ import numpy
 
 '''
 '''
-
-
 class Linear:
 
-    '''
-    '''
-    m_ValidTypes = (int, float, list, numpy.matrix, numpy.ndarray)
-    m_Weights    = None
-    m_Bias       = None
+  '''
+  '''
+  m_ValidTypes = ( int, float, list, numpy.matrix, numpy.ndarray )
+  m_Weights    = None
+  m_Bias       = None
 
-    '''
-    '''
-    def __init__(self, w, b=None):
+  '''
+  '''
+  def __init__( self, w, b = None ):
+    
+    assert isinstance( w, self.m_ValidTypes ), \
+      'Invalid type for model weights'
 
-        assert isinstance(w, self.m_ValidTypes), \
-            'Invalid type for model weights'
+    theta = numpy.matrix( w )
+    assert theta.shape[ 0 ] == 1, 'Parameters should be a row vector.'
 
-        theta = numpy.matrix(w)
-        assert theta.shape[0] == 1, 'Parameters should be a row vector.'
+    mb = None
+    if b != None:
+      assert isinstance( b, self.m_ValidTypes ), \
+        'Invalid type for model bias'
 
-        mb = None
-        if b:
-            assert isinstance(b, self.m_ValidTypes), \
-                'Invalid type for model bias'
+      self.m_Weights = theta
+      mb = numpy.matrix( b )
 
-            self.m_Weights = theta
-            mb = numpy.matrix(b)
+      assert mb.shape[ 0 ] == 1 and mb.shape[ 1 ] == 1, \
+        'Invalid parameters sizes.'
+      
+    else:
+      self.m_Weights = theta[ :, 1 : ]
+      mb = theta[ :, : -1 ]
+    # end if
 
-            assert mb.shape[0] == 1 and mb.shape[1] == 1, \
-                'Invalid parameters sizes.'
+    self.m_Bias = mb[ 0, 0 ]
 
-        else:
-            self.m_Weights = theta[:, 1:]
-            mb = theta[:, : -1]
-        # end if
+  # end def
 
-        self.m_Bias = mb[0, 0]
+  '''
+  '''
+  def Dimensions( self ):
+    return self.m_Weights.shape[ 1 ]
+  # end def
 
-    # end def
+  '''
+  '''
+  def __call__( self, *cargs ):
+    assert len( cargs ) > 0, 'No arguments passed to __call__()'
 
-    '''
-    '''
-    def Dimensions(self):
-        return self.m_Weights.shape[1]
-    # end def
+    if len( cargs ) == 1:
+      assert isinstance( cargs[ 0 ], self.m_ValidTypes )
+      if isinstance( cargs[ 0 ], ( numpy.matrix ) ):
+        x = cargs[ 0 ]
+      else:
+        x = numpy.matrix( cargs[ 0 ] )
+      # end if
+    else:
+      x = numpy.matrix( list( cargs ) )
+    # end if
 
-    '''
-    '''
-    def __call__(self, *cargs):
-        assert len(cargs) > 0, 'No arguments passed to __call__()'
+    assert x.shape[ 1 ] == self.m_Weights.shape[ 1 ], \
+      'Input size is not compatible.'
 
-        if len(cargs) == 1:
-            assert isinstance(cargs[0], self.m_ValidTypes)
-            if isinstance(cargs[0], (numpy.matrix)):
-              x = cargs[0]
-            else:
-              x = numpy.matrix(cargs[0])
-            # end if
-        else:
-            x = numpy.matrix(list(cargs))
-        # end if
+    return ( ( self.m_Weights @ x.T ) + self.m_Bias ).T
 
-        assert x.shape[1] == self.m_Weights.shape[1], \
-            'Input size is not compatible.'
-
-        return ((self.m_Weights @ x.T) + self.m_Bias).T
-
-    # end def
+  # end def
 
 # end class
 
-# eof - $RCSfile$
+## eof - $RCSfile$
--- lib/python3/PUJ/Model/Logistic.py
+++ lib/python3/PUJ/Model/Logistic.py
@@ -7,26 +7,24 @@ from .Linear import *
 
 '''
 '''
+class Logistic( Linear ):
 
+  '''
+  '''
+  def __init__( self, w, b = None ):
+    super( ).__init__( w, b )
+  # end def
 
-class Logistic(Linear):
-    '''
-    '''
-
-    def __init__(self, w, b=None):
-        super().__init__(w, b)
-    # end def
-
-    '''
-    '''
-    def __call__(self, *cargs, threshold=True):
-        z = 1.0 / (1.0 + numpy.exp(-super().__call__(*cargs)))
-        if threshold:
-            return (z >= 0.5).astype(z.dtype)
-        else:
-            return z
-        # end if
-    # end def
+  '''
+  '''
+  def __call__( self, *cargs, threshold = True ):
+    z = 1.0 / ( 1.0 + numpy.exp( -super( ).__call__( *cargs ) ) )
+    if threshold:
+      return ( z >= 0.5 ).astype( z.dtype )
+    else:
+      return z
+    # end if
+  # end def
 
 # end class
 
